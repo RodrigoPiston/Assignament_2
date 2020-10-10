d0418d0fd16bede8fbc99f4d0bb39bd9c8a17465
Rodrigo Pistón <heidozanzo@live.com>
2020-10-10 12:18:06 -0300

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatDialogModule } from '@angular/material/dialog'; 
 
 // -- Services
 import { DishService } from './services/dish.service';
@@ -27,6 +28,7 @@ import { ContactComponent } from './contact/contact.component';
 
 // -- Independent Imports
 import 'hammerjs';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -37,18 +39,24 @@ import 'hammerjs';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    AppRoutingModule,
     FlexLayoutModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
+  // -- For show the modals dialog
+  entryComponents: [
+    LoginComponent
   ],
   providers: [ DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
